738ceaaaf03cad81f3306152bcacaf86d8695973
Lucas Fonseca <137899570+lucasedufonseca@users.noreply.github.com>
2025-05-16 08:51:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -53,156 +53,160 @@ DECLARE
     codCondicaoComercial NUMBER;
     isencaoDeTaxa VARCHAR2(1);
     v_exists NUMBER;
-BEGIN
+    
+    BEGIN
+    
     gestorPortal := 2;
-    usuarioIntegracao1 := 'nws.integracao';
-    usuarioIntegracao2 := 'nws.integracao2';
+    usuarioIntegracao1 :=  'nws.integracao';
+    usuarioIntegracao2 :=  'nws.integracao2';
     
     -- INICIO DADOS CUSTOMIZAVEIS
-    gestorEntityId := {gestorEntityId};
-    gestorNome := '{gestorNome}';
-    storeNome := '{storeNome}';
-    numeroRegistroJunta := '{numeroRegistroJunta}';
-    {gestorId_block}
-    gestorLogo := '{gestorLogo}';
-    storeUri := '{storeUri}';
-    gestorContactEmail := '{gestorContactEmail}';
-    gestorTabela := {gestorTabela};
-    isencaoDeTaxa := '{isencaoDeTaxa}';
+    gestorEntityId := 447789; --ID de cadastro PF/PJ responsável pelo gestor
+    gestorNome := 'VIVIANE GARZON CORRÊA'; --Nome comercial desejado pelo gestor
+    storeNome := 'Bolsa de Leilõess'; --Nome comercial para a loja
+    numeroRegistroJunta := '387'; --Num. registro do leiloeiro (se não for informado no ticket remover essa linha)
+    SELECT NVL(MAX(GESTOR_ID), 0) + 1 INTO gestorId FROM GESTOR; -- Valor obtido do select da tabela gestor 
+    gestorLogo := 'https://static.s4bdigital.net/logos_empresas/bolsa_de_leiloes.png'; --Logo da loja no CDN SBWS
+    storeUri := 'http://www.bolsadeleiloesbh.com.br/'; --URL da loja manter sempre protocolo + hostname
+    gestorContactEmail := 'atendimentosp@bolsadeleiloes.com.br'; --E-mail de contato para a loja
+    gestorTabela := 4; --Tabela informada no ticket 1, 2 ou 3
+    isencaoDeTaxa:= 'N';
+    
     -- FIM DADOS CUSTOMIZAVEIS
-
+    
     -- ETAPA 1 - CRIAR GESTOR
+    
     -- Atribuir papel de gestor para entity
     SELECT COUNT(*) INTO v_exists FROM REL_ROLE_ENTITY WHERE ENTITY_ID = gestorEntityId AND ROLE_ID = 40;
-    IF v_exists = 0 THEN
+      IF v_exists = 0 THEN
         INSERT INTO REL_ROLE_ENTITY (ENTITY_ID, ROLE_ID)
         VALUES (gestorEntityId, 40);
-    END IF;
-
+      END IF;
+    
     SELECT COUNT(*) INTO v_exists FROM INTER_ENTITY WHERE ENTITY_ID = gestorEntityId AND ENTITY_PARENT_ID = gestorEntityId AND ROLE_ID = 40;
-    IF v_exists = 0 THEN
+      IF v_exists = 0 THEN
         INSERT INTO INTER_ENTITY (ENTITY_ID, ENTITY_PARENT_ID, ROLE_ID)
         VALUES (gestorEntityId, gestorEntityId, 40);
-    END IF;
-
+      END IF;
+    
     -- Cadastrar gestor 
     INSERT INTO GESTOR (GESTOR_ID, GESTOR, ENTITY_ID, GESTOR_TITLE, GESTOR_DESC, GESTOR_INATIVO, EREG_PORTAL_ID)
     VALUES (gestorId, gestorNome, gestorEntityId, gestorNome, gestorNome, 'F', gestorPortal);
     --desc gestor;
         
     INSERT INTO GESTOR_SETTINGS (GESTOR_ID,SHOW_ITENS_PORCENTAGEM,COUNTRY_CODE,COD_ERP_SYSTEM,ERP_KEY,AUTO_SALE_START,SYSTEM_CONTACT_EMAIL,AUTO_SALE_GENERATE,GESTOR_CONTACT_EMAIL,COD_PROJETO_LEILAO, PLATFORM_LICENSOR_ID) 
-    values (gestorId,'2,3','1',2,'CORPORE_RM','01/01/1900','transaction-team@superbid.net','T',gestorContactEmail,165064, 3);
-
+      values (gestorId,'2,3','1',2,'CORPORE_RM','01/01/1900','transaction-team@superbid.net','T',gestorContactEmail,165064, 3);
+    
     INSERT INTO GESTOR_GEO_APPROVAL_DEFAULT (EREF_COUNTRY_CODE, GESTOR_ID) VALUES (1,gestorId);
-
+    
     -- Projeto Comercial (61922 - Superbid Webservices)
     INSERT INTO COMMERCIAL_PROJECT (COMMERCIAL_PROJECT_DESC,COMMERCIAL_PROJECT_STATUS_ID,OWNER_ID,YOUR_REFERENCE,CREATED_AT,UPDATED_AT,CLOSE_AT,GESTOR_ID,ECONOMIC_GROUP_ID)
-        values ('PROJETO PADRAO','1','61922','',SYSDATE,null,SYSDATE,gestorId,null);
-
+         values ('PROJETO PADRAO','1','61922','',SYSDATE,null,SYSDATE,gestorId,null);
+    
     -- Dar acesso as APIS ao usuario do administrativo ( GATE , Plataforma Leiloes, etc.. )
     SELECT PERFIL_ID INTO usuarioIntegracao1PerfilId FROM PERFIL WHERE ENTITY_ID IN (SELECT ENTITY_ID FROM LOGIN WHERE LOGIN_NAME = usuarioIntegracao1) AND EREG_PORTAL_ID = gestorPortal;
-
+    
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao1PerfilId,'PRG0002',SYSDATE,35,gestorId); -- Operador do Pregão
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao1PerfilId,'CDV0002',SYSDATE,36,gestorId); -- Operador Condicao de Venda
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao1PerfilId,'LOT0002',SYSDATE,11,gestorId); -- Operador do modulo loteamento Superbid
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao1PerfilId,'CCF',SYSDATE,71,gestorId); -- Adminstrador do Modulo
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao1PerfilId,'CPC',SYSDATE,106,gestorId); -- Administrador do Módulo
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao1PerfilId,'CDA',SYSDATE,111,gestorId); -- Adminstrador CDA - GATE 1.0
-
+    
     -- Dar acesso as APIS ao usuario do administrativo ( GATE , Plataforma Leiloes, etc.. )
     SELECT PERFIL_ID INTO usuarioIntegracao2PerfilId FROM PERFIL WHERE ENTITY_ID IN (SELECT ENTITY_ID FROM LOGIN WHERE LOGIN_NAME = usuarioIntegracao2) AND EREG_PORTAL_ID = gestorPortal;
-
+    
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao2PerfilId,'PRG0002',SYSDATE,35,gestorId); -- Operador do Pregão
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao2PerfilId,'CDV0002',SYSDATE,36,gestorId); -- Operador Condicao de Venda
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao2PerfilId,'LOT0002',SYSDATE,11,gestorId); -- Operador do modulo loteamento Superbid
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao2PerfilId,'CCF',SYSDATE,71,gestorId); -- Adminstrador do Modulo
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao2PerfilId,'CPC',SYSDATE,106,gestorId); -- Administrador do Módulo
     INSERT INTO PERFIL_PERMISSAO (PERFIL_ID, MODULO_ID, CREATION_DATE, PAPEL_ID, GESTOR_ID)  VALUES (usuarioIntegracao2PerfilId,'CDA',SYSDATE,111,gestorId); -- Adminstrador CDA - GATE 1.0
-
+    
     -- SETUP TRANSACIONAL FINANCEIRO
-
+    
     -- Forma de Pagamento
     INSERT INTO FL_FORMA_PAGAMENTO_GESTOR VALUES(gestorId,2); --Depósito
     INSERT INTO FL_FORMA_PAGAMENTO_GESTOR VALUES(gestorId,9); --S4Payments
     INSERT INTO FL_FORMA_PAGAMENTO_GESTOR VALUES(gestorId,11); --Guia de depósito Judicial
-
+    
     -- Itens de Pagamento - 1º Split
     INSERT INTO FL_TIPO_ITEM_PAG_GESTOR(COD_GESTOR, COD_TIPO_ITEM_PAGAMENTO, DATA_CRIACAO, STATUS, ORDEM_BAIXA) VALUES(gestorId, 1, SYSDATE, 'T', 3); --Pagamento do lote
     INSERT INTO FL_TIPO_ITEM_PAG_GESTOR(COD_GESTOR, COD_TIPO_ITEM_PAGAMENTO, DATA_CRIACAO, STATUS, ORDEM_BAIXA) VALUES(gestorId, 2, SYSDATE, 'T', 1); --Comissão do leiloeiro
     INSERT INTO FL_TIPO_ITEM_PAG_GESTOR(COD_GESTOR, COD_TIPO_ITEM_PAGAMENTO, DATA_CRIACAO, STATUS, ORDEM_BAIXA) VALUES(gestorId, 5, SYSDATE, 'T', 2); --Encargos de Administração
     INSERT INTO FL_TIPO_ITEM_PAG_GESTOR(COD_GESTOR, COD_TIPO_ITEM_PAGAMENTO, DATA_CRIACAO, STATUS, ORDEM_BAIXA) VALUES(gestorId, 3, SYSDATE, 'T', 2); --Buyers
-
+    
     -- Itens de Serviço - 2º Split
-
+    
     IF isencaoDeTaxa != 'S' THEN
-
+    
     INSERT INTO FL_TIPO_ITEM_SERV_GESTOR(COD_GESTOR, COD_TIPO_ITEM_SERVICO, STATUS) VALUES(gestorId, 1, 'T'); --Vendedor
     INSERT INTO FL_TIPO_ITEM_SERV_GESTOR(COD_GESTOR, COD_TIPO_ITEM_SERVICO, STATUS) VALUES(gestorId, 2, 'T'); --Licenciamento
     INSERT INTO FL_TIPO_ITEM_SERV_GESTOR(COD_GESTOR, COD_TIPO_ITEM_SERVICO, STATUS) VALUES(gestorId, 3, 'T'); --Assessoria
-
+    
     ELSE
-
+    
     INSERT INTO FL_TIPO_ITEM_SERV_GESTOR(COD_GESTOR, COD_TIPO_ITEM_SERVICO, STATUS) VALUES(gestorId, 1, 'T'); --Vendedor
     INSERT INTO FL_TIPO_ITEM_SERV_GESTOR(COD_GESTOR, COD_TIPO_ITEM_SERVICO, STATUS) VALUES(gestorId, 3, 'T'); --Assessoria
-
+    
     END IF;
-
+    
     -- SETUP LOJA
     INSERT INTO SBINS.STORE (ID, NAME,CREATED_AT,UPDATED_AT,PORTAL_ID,VISIBLE_ON_PORTAL,HIGHLIGHT,LOGO_URI,DESCRIPTION,POSITION,EXTERNAL_LINK,VISIBLE_ON_PORTAL_BANNER,LAST_UPDATE_DATE,STORE_URI) 
-    values ((SELECT MAX(ID)+1 FROM STORE),UPPER(storeNome),SYSDATE,SYSDATE,15,'T','T',gestorLogo,UPPER(storeNome),null,null,'T',SYSDATE,storeUri) RETURNING ID INTO storeId;
-
+      values ((SELECT MAX(ID)+1 FROM STORE),UPPER(storeNome),SYSDATE,SYSDATE,15,'T','T',gestorLogo,UPPER(storeNome),null,null,'T',SYSDATE,storeUri) RETURNING ID INTO storeId;
+    
     INSERT INTO REL_STORE_EVENT_MANAGER ( STORE_ID, EVENT_MANAGER_ID ) VALUES ( storeId, gestorId );
-
+    
     -- SETUP LEILOEIRO
     IF numeroRegistroJunta IS NOT NULL THEN
         SELECT COUNT(*) INTO v_exists FROM INTER_ENTITY WHERE ENTITY_ID = gestorEntityId AND ENTITY_PARENT_ID = gestorEntityId AND ROLE_ID = 11;
         IF v_exists = 0 THEN
-        INSERT INTO INTER_ENTITY (ENTITY_ID, ENTITY_PARENT_ID, ROLE_ID)
-        VALUES (gestorEntityId, gestorEntityId, 11);
+          INSERT INTO INTER_ENTITY (ENTITY_ID, ENTITY_PARENT_ID, ROLE_ID)
+          VALUES (gestorEntityId, gestorEntityId, 11);
         END IF;
-
+    
         SELECT COUNT(*) INTO v_exists FROM DOCUMENT WHERE DOCUMENT_NUMBER = numeroRegistroJunta AND DOCUMENT_TYPE_ID = 6 AND ENTITY_ID = gestorEntityId;
         IF v_exists = 0 THEN
-        INSERT INTO DOCUMENT (DOCUMENT_NUMBER, DOCUMENT_TYPE_ID, ENTITY_ID)
-        VALUES (numeroRegistroJunta, 6, gestorEntityId);
+          INSERT INTO DOCUMENT (DOCUMENT_NUMBER, DOCUMENT_TYPE_ID, ENTITY_ID)
+          VALUES (numeroRegistroJunta, 6, gestorEntityId);
         END IF;
-
+    
         SELECT COUNT(*) INTO v_exists FROM REL_GESTOR_AUCTIONEER WHERE GESTOR_ID = gestorId AND ENTITY_ID = gestorEntityId;
         IF v_exists = 0 THEN
-        INSERT INTO REL_GESTOR_AUCTIONEER (GESTOR_ID, ENTITY_ID)
-        VALUES (gestorId, gestorEntityId);
+          INSERT INTO REL_GESTOR_AUCTIONEER (GESTOR_ID, ENTITY_ID)
+          VALUES (gestorId, gestorEntityId);
         END IF;
     END IF;
-
+    
     -- NOVO PERMISSIONAMENTO FINLEI
     INSERT INTO gestor_acesso (GESTOR_ID,GESTOR_ID_PAI) VALUES(gestorId,114);
-
+    
     IF isencaoDeTaxa != 'S' THEN
-
+    
     -- CRIAR CONDICAO DE PLATAFORMA
     insert into fl_ccvenda (COD_CONDICAO_COMERCIAL,DESC_CONDICAO_COMERCIAL,COD_TIPO_TRANSACAO,COD_GESTOR,COD_VENDEDOR,COD_EVENTO,COD_OFERTA,COD_FORMA_PAGAMENTO,COD_FAVORECIDO,DIAS_VENCIMENTO,INSTRUCAO_PAGAMENTO,DATA_CRIACAO,DATA_ATUALIZACAO,COD_STATUS,MIN_PORCENTAGEM_ENTRADA,MAX_QUANTIDADE_PARCELAS,AFTER_CASHOUT,COBRANCA_PLATAFORMA,DATA_EXPIRACAO,DIAS_CONDICIONAL,TIPO_FAVORECIDO)
-    values ((SELECT MAX(COD_CONDICAO_COMERCIAL)+1 FROM fl_ccvenda),gestorId || ' ' || gestorNome || ' - TABELA ' || gestorTabela,1,gestorId,null,null,null,null,null,null,null,SYSDATE,SYSDATE,2,null,null,null,'T',null,null,null) RETURNING COD_CONDICAO_COMERCIAL INTO codCondicaoComercial;
-
+      values ((SELECT MAX(COD_CONDICAO_COMERCIAL)+1 FROM fl_ccvenda),gestorId || ' ' || gestorNome || ' - TABELA ' || gestorTabela,1,gestorId,null,null,null,null,null,null,null,SYSDATE,SYSDATE,2,null,null,null,'T',null,null,null) RETURNING COD_CONDICAO_COMERCIAL INTO codCondicaoComercial;
+    
     insert into fl_ccvenda_item (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO) values (codCondicaoComercial,2);
-
+    
     insert into fl_ccvenda_split (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,COD_FAVORECIDO,COD_TIPO_COBRANCA,COD_BASE_CALCULO,PORCENTAGEM,VALOR,VALOR_MAXIMO)
-    values (codCondicaoComercial,2,2,61922,3,1,null,null,null);
-
+      values (codCondicaoComercial,2,2,61922,3,1,null,null,null);
+    
     ELSE
-
+    
     insert into fl_ccvenda (COD_CONDICAO_COMERCIAL,DESC_CONDICAO_COMERCIAL,COD_TIPO_TRANSACAO,COD_GESTOR,COD_VENDEDOR,COD_EVENTO,COD_OFERTA,COD_FORMA_PAGAMENTO,COD_FAVORECIDO,DIAS_VENCIMENTO,INSTRUCAO_PAGAMENTO,DATA_CRIACAO,DATA_ATUALIZACAO,COD_STATUS,MIN_PORCENTAGEM_ENTRADA,MAX_QUANTIDADE_PARCELAS,AFTER_CASHOUT,COBRANCA_PLATAFORMA,DATA_EXPIRACAO,DIAS_CONDICIONAL,TIPO_FAVORECIDO)
-    values ((SELECT MAX(COD_CONDICAO_COMERCIAL)+1 FROM fl_ccvenda),gestorId || ' ' || gestorNome || ' - TABELA ' || gestorTabela,1,gestorId,null,null,null,null,null,null,null,SYSDATE,SYSDATE,3,null,null,null,'T',null,null,null) RETURNING COD_CONDICAO_COMERCIAL INTO codCondicaoComercial;
-
+      values ((SELECT MAX(COD_CONDICAO_COMERCIAL)+1 FROM fl_ccvenda),gestorId || ' ' || gestorNome || ' - TABELA ' || gestorTabela,1,gestorId,null,null,null,null,null,null,null,SYSDATE,SYSDATE,3,null,null,null,'T',null,null,null) RETURNING COD_CONDICAO_COMERCIAL INTO codCondicaoComercial;
+    
     insert into fl_ccvenda_item (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO) values (codCondicaoComercial,2);
-
+    
     insert into fl_ccvenda_split (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,COD_FAVORECIDO,COD_TIPO_COBRANCA,COD_BASE_CALCULO,PORCENTAGEM,VALOR,VALOR_MAXIMO)
-    values (codCondicaoComercial,2,2,61922,3,1,null,null,null);
-
+      values (codCondicaoComercial,2,2,61922,3,1,null,null,null);
+    
     END IF;
-
+    
     -- INICIO TABELA 1
     IF gestorTabela = 1 THEN
-
+    
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
             values (codCondicaoComercial,'2','2','3000,01','10000',null,'50');
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
@@ -211,14 +215,14 @@ BEGIN
             values (codCondicaoComercial,'2','2','30000,01','100000',null,'250');
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
             values (codCondicaoComercial,'2','2','100000,01','999999999999',null,'350');
-
+    
     END IF;
-
+    
     -- FIM TABELA 1
-
+    
     -- INICIO TABELA 2
     IF gestorTabela = 2 THEN
-
+    
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
             values (codCondicaoComercial,'2','2','3000,01','10000',null,'50');
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
@@ -229,14 +233,14 @@ BEGIN
             values (codCondicaoComercial,'2','2','100000,01','300000',null,'600');
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
             values (codCondicaoComercial,'2','2','300000,01','999999999999',null,'1000');
-
+    
     END IF;
-
+    
     -- FIM TABELA 2
-
+    
     -- INICIO TABELA 3
     IF gestorTabela = 3 THEN
-
+    
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
             values (codCondicaoComercial,'2','2','0','10000',null,'50');
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
@@ -247,14 +251,14 @@ BEGIN
             values (codCondicaoComercial,'2','2','100000,01','300000',null,'600');
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
             values (codCondicaoComercial,'2','2','300000,01','999999999999',null,'1000');
-
+    
     END IF;
-
+    
     -- FIM TABELA 3
-
+    
     -- INICIO TABELA 4
     IF gestorTabela = 4 THEN
-
+    
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
             values (codCondicaoComercial,'2','2','0','10000',null,'10');
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
@@ -265,11 +269,11 @@ BEGIN
             values (codCondicaoComercial,'2','2','100000,01','300000',null,'600');
         insert into fl_ccvenda_split_faixa (COD_CONDICAO_COMERCIAL,COD_TIPO_ITEM_PAGAMENTO,COD_TIPO_ITEM_SERVICO,VALOR_INICIAL,VALOR_FINAL,PORCENTAGEM,VALOR)
             values (codCondicaoComercial,'2','2','300000,01','999999999999',null,'1000');
-
+    
     END IF;
-
+    
     -- FIM TABELA 4
-
+    
     END;
     /
 """,
